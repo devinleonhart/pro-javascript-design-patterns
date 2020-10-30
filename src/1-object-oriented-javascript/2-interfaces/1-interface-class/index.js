ef8b06c4a73c0a7eba30dc795b82d90ed33a6bca
Devin Leonhart <devinleonhart@fastmail.com>
2020-10-30 14:11:37 -0700

    Added 2-1

--- src/1-object-oriented-javascript/2-interfaces/1-interface-class/index.js
+++ src/1-object-oriented-javascript/2-interfaces/1-interface-class/index.js
@@ -1,1 +1,69 @@
-module.exports = {};
+/*
+  Javascript does not natively support the Interface pattern. We have to
+  implement the Interface class ourselves to use the pattern.
+*/
+const Interface = function(name, methods) {
+  if (arguments.length !== 2) {
+    throw new Error(
+      'Interface constructor called with ' +
+      arguments.length +
+      ' arguments, but expected exactly 2'
+    );
+  }
+
+  if (typeof name !== 'string') {
+    throw new Error(
+      'Interface constructor expects interface name to be a string.' +
+      'Received' + typeof name + '.'
+    );
+  }
+
+  for (let i = 0; i < methods.length; i++) {
+    if (typeof methods[i] !== 'string') {
+      throw new Error(
+        'Interface constructor expects method names to be a string.' +
+        'Received' + typeof method + '.'
+      );
+    }
+  }
+
+  this.name = name;
+  this.methods = methods;
+};
+
+Interface.ensureImplements = function(object) {
+  if (arguments.length < 2) {
+    throw new Error(
+      'Function Interface.ensureImplements called with ' +
+      arguments.length +
+      ' arguments, but expected at least 2.'
+    );
+  }
+
+  for (let i = 1; i < arguments.length; i++) {
+    const inter = arguments[i];
+
+    if (inter.constructor !== Interface) {
+      throw new Error(
+        'Function Interface.ensureImplements expects arguments 2 and above to' +
+        'be instances of Interface.'
+      );
+    }
+
+    for (let j = 0; j < inter.methods.length; j++) {
+      const method = inter.methods[j];
+      if (!object[method] || typeof object[method] !== 'function') {
+        throw new Error(
+          'Function Interface.ensureImplements: object does not implement the ' +
+          inter.name +
+          ' interface. Method ' +
+          method +
+          ' was not found.'
+        );
+      }
+    }
+  }
+  return true;
+};
+
+module.exports = Interface;
--- test/1-2-1-interface-class.js
+++ test/1-2-1-interface-class.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 
 const chapter = chapter2.chapter;
 const name = chapter2.name;
-const test = chapter2.test.section1;
+const Interface = chapter2.test.section1;
 
 describe('Chapter 2 - Section 1', function() {
   it('should be the right chapter', function() {
@@ -15,12 +15,29 @@ describe('Chapter 2 - Section 1', function() {
   });
 
   describe('interface class', function() {
-    it('should have a name', function() {
-      expect(name).to.equal('interfaces');
+    it('should check for implemented methods', function() {
+      const StopWatch = function() {
+        this.start = function() { return 'Stubbed Start'; };
+        this.stop = function() { return 'Stubbed Stop'; };
+      };
+      const sw = new StopWatch();
+      const inter = new Interface('StopWatch', ['start', 'stop']);
+      const badInter = new Interface('StopWatch', ['pizza', 'bagel']);
+
+      expect(inter.constructor).to.equal(Interface);
+      expect(() => Interface.ensureImplements(sw, badInter)).to.throw();
     });
 
-    it('should have exported something', function() {
-      expect(test).to.be.a('object');
+    it('should throw on bad interface', function() {
+      const StopWatch = function() {
+        this.start = function() { return 'Stubbed Start'; };
+        this.stop = function() { return 'Stubbed Stop'; };
+      };
+      const sw = new StopWatch();
+      const badInter = new Interface('StopWatch', ['pizza', 'bagel']);
+
+      expect(badInter.constructor).to.equal(Interface);
+      expect(() => Interface.ensureImplements(sw, badInter)).to.throw(/pizza/);
     });
   });
 });
